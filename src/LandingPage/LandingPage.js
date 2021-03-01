e176c0d8a621aa0cd369b3b6ce6a10b6ec062735
Matt Sirkis <sirkis.matt@gmail.com>
2021-03-01 17:25:32 +1300

    edit landing page css

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,16 +1,16 @@
-import React, {useState, useLayoutEffect} from 'react';
+import React, {useState, useLayoutEffect, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import './LandingPage.css'
 import LandingNav from '../LandingNav/LandingNav'
-import growLaptop from '../images/Grow_Laptop.png'
-import growthPhone from '../images/GrowthPhone_Mock.png'
+import Axios from 'axios'
 import kauriLoop from '../video/kauriLoop.mp4'
 
 
 
 function LandingPage(props) {
 
-  const [size, setSize] = useState([0, 0]);
+  const [size, setSize] = useState([0, 0])
+  const [treeCount, setCount] = useState(0)
 
   useLayoutEffect(() => {
     function updateSize() {
@@ -20,6 +20,19 @@ function LandingPage(props) {
     updateSize();
     return () => window.removeEventListener('resize', updateSize);
     }, []);
+
+
+    useEffect(() => {
+      Axios.get(`https://api-dev.digitalhumani.com/enterprise/7997dd50/treeCount?startDate=2020-11-00&endDate=2030-01-01`)
+      .then(res => {
+        console.log(res.data)
+        setCount(res.data.count)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  
+    }, [])
 
     const [width, height] = size
 
@@ -31,71 +44,58 @@ function LandingPage(props) {
             width: width
           }}>
             <LandingNav/>
-            <video autoPlay loop muted className="kauri_video" 
+            <video autoPlay loop muted className="kauri-video" 
           
           >
               <source src={kauriLoop} type="video/mp4"/>
             </video>
             <div className="overlay"></div>
             <div className="shadow-overlay"></div>
-            <div className="overlay_text">
-              <h3 id="catch_phrase">Better You Better Planet</h3>
-              <h1>Grow lets you set goals for yourself and keeps you accountable.</h1>
-              <button className="signup_btn_main"><Link to='/signup' className="sign-up">Sign Up - Start Growing</Link></button>
+            <div className="overlay-text">
+              <h3 id="catch-phrase">Better You Better Planet</h3>
+              <h1 className="desc-title">Grow lets you set goals for yourself and keeps you accountable.</h1>
+              <button className="signup-btn-main"><Link to='/signup' className="sign-up">Sign Up - Start Growing</Link></button>
             </div>
         </div>
-        
-        <div className='landing_main'>
-          <section className='landing-section'>
-            <div className="landing-row">
-              <div className="column-1">  
-                <h3>Set goals for yourself</h3>
-                <p>Grow lets you set goals for yourself and keeps you accountable.</p>
+        <div className="about" >
+          <div className="row section-header has-bottom-sep">
+            <div className="col-full">
+              <h3 className="sub-head">What we are about</h3>
+              <h1 className="display-1">Healthy Planet and Happy People</h1>
+            </div>
+          </div>
+          <div className="row about-section">
+            <div className="col-full">
+              <p>
+              Succeed whether you meet your goals or not. You are either benefitting yourself or the planet! 
+              Set how many trees you will donate if you don't complete your goal.
+              </p>
+              <p>If you don't complete your goal, you will be asked to donate 1$ per tree by a trusted reforestation 
+                  organizations. We use RaaS to handle the donation process. The API is powered by 
+                  <a id="DigitalHumani"href="https://digitalhumani.com/" target="blank"> Digitalhumani. </a>
+              </p>
+            </div>
+          </div>
+          <div className="row about-stats stats">
+            <div className="col-block stats-col">
+              <div className="stats-count">
+                {treeCount}
               </div>
-              <div className="column-2">
-                <img id="growthPhone" src={growthPhone} alt="A screenshot of the dashboard on an iphone" />
-              </div>
+              <h5>Trees Planted</h5>
+            </div>
+          </div>
+        </div>
+        <footer>
+          <div className="row footer-main">
+            <div className="footer-bottom">
+            <div className="info">
+              <p>Developed by: Matt Sirkis</p>
+              <p><a href="https://www.linkedin.com/in/matt-sirkis/" target="blank">LinkedIn</a></p>
+            </div>
             </div>
             
-          </section>
-
-          <hr/>
-
-          <section className='landing-section image_only'>
-            <div className="img_container">
-              <div>
-              <img src={growLaptop} alt="A screenshot of trees being donated"/>
-              </div>
-              <div className="overlay_second_img">
-                <h3>Plant trees if you fall short on tasks you set for yourself</h3>
-              </div>
-              {/* <div className="column-2">
-                <img src={growLaptop} alt="A screenshot of trees being donated"/>
-              </div>
-              <div className="column-1">
-                <h3>Plant trees if you fall short on tasks you set for yourself</h3>
-                <p>Succeed whether you meet your goals or not. You are either benefitting yourself or the planet! Set how many trees you will donate if you don't complete your goal.</p>
-              </div> */}
-            </div>
-          </section>
-
-          <hr/>
-
-          <section className='landing-section'>
-            <div className="landing-row">
-              <div className="column-1">
-                <h3>How does the tree planting service work?</h3>
-                <p>If you don't complete your goal, you will be asked to donate 1$ per tree by trusted reforestation 
-                  organizations (One Tree Planted, WeForest, TIST or Sustainable Harvest International). We use RaaS to handle the donation process. The API is powered by 
-                  <a id="DigitalHumani"href="https://digitalhumani.com/" target="blank"> Digitalhumani. </a></p>
-              </div>
-              {/* <div className="column-2">
-                <img id="Raas" src={Raas} alt="A picture of Raas"/>
-              </div> */}
-            </div>
-          </section>
-        </div>
-   
+          </div>
+        </footer>
       </div>
     );
 
